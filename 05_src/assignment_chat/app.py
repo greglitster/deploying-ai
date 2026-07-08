81fb3a72ca4a74ee0f925c259ac932360e9ca924
Greg Litster <greglitster3@hotmail.com>
2026-07-08 15:18:54 -0400

    adding in an python app file to be able to write the app

---
+++ 05_src/assignment_chat/app.py
@@ -0,0 +1,29 @@
+import gradio as gr
+from langchain_core.messages import HumanMessage, AIMessage
+from dotenv import load_dotenv
+from typing import Optional
+import os
+
+from langchain.chat_models import init_chat_model
+
+load_dotenv('.secrets')
+
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+def simple_chat(message: str, history: list[dict]) -> str:
+    langchain_messages = []
+    for msg in history:
+        if msg['role'] == 'user':
+            langchain_messages.append(HumanMessage(content=msg['content']))
+        elif msg['role'] == 'assistant':
+            langchain_messages.append(AIMessage(content=msg['content']))
+    langchain_messages.append(HumanMessage(content=message))
+
+    response = llm.invoke(langchain_messages)
+
+    return response.content
+
+
+gr.ChatInterface(
+    fn=simple_chat,
+).launch()
